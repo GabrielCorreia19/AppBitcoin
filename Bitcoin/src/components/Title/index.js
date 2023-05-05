a0f1bfaa7ec010abd302284231bb5f25f18b8aab
Gabriel Correia <gabriel.com.gc10@gmail.com>
2023-05-05 15:52:51 -0300

    subindo codigo feito na empresa na data de 05/05

---
+++ Bitcoin/src/components/Title/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {View, Text} from "react-native";
+
+export default function Title(){
+    return (
+        <View>
+            <Text>ONBITHEALTH...</Text>
+        </View>
+    )
+}
+
+
+w
